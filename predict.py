92b30745b9c6bcb140dead314083d64a9f8b008e
joe <joe@replicate.com>
2024-01-24 22:06:47 +0000

    Add TRTLLMClient and tokenizer, launch Triton server

--- predict.py
+++ predict.py
@@ -1,23 +1,38 @@
 # Prediction interface for Cog ⚙️
 # https://github.com/replicate/cog/blob/main/docs/python.md
 
-from cog import BasePredictor, Input, Path
+from cog import BasePredictor, Input, Path, ConcatenateIterator
 from utils import maybe_download_tarball_with_pget
+import subprocess
+from trtllm_client import TRTLLMClient
+from transformers import AutoTokenizer, LlamaTokenizer, T5Tokenizer
 
 URL = "https://replicate.delivery/pbxt/qkRFtudUXCoAKlntnVLc3dBhRutRoW02L127bU3Q4778emHJA/engine.tar"
 
 class Predictor(BasePredictor):
     def setup(self) -> None:
-        print("Downloading model files...")
-        maybe_download_tarball_with_pget(
-            url=URL,
-            dest="./engine",
-        )
+        # print("Downloading model files...")
+        # maybe_download_tarball_with_pget(
+        #     url=URL,
+        #     dest="./engine",
+        # )
+        # launch triton server
+        # python3 scripts/launch_triton_server.py --world_size=1 --model_repo=/src/tensorrtllm_backend/triton_model
+        subprocess.run(["python3", "/src/tensorrtllm_backend/scripts/launch_triton_server.py", "--world_size=1", "--model_repo=/src/tensorrtllm_backend/triton_model"])
         
+        tokenizer = AutoTokenizer.from_pretrained(
+            "/src/tensorrtllm_backend/tensorrt_llm/examples/gpt/gpt2",
+            padding_side='left',
+            trust_remote_code=True
+        )
 
+        self.client = TRTLLMClient(tokenizer=tokenizer)
+        
     def predict(
         self,
         prompt: str = Input(description="Enter a prompt", default=""),
-    ) -> str:
+    ) -> ConcatenateIterator:
 
-        return "Hello world!"
+        for decoded_token in self.client.run(text=prompt):
+            yield decoded_token
+
