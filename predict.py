f0629db9a106c82ef17ae2333ed456dd63c2c329
joe <joe@replicate.com>
2024-01-31 22:11:32 +0000

    Update predict.py with TRTLLMClient and model repository path

--- predict.py
+++ predict.py
@@ -9,6 +9,7 @@ from transformers import AutoTokenizer, LlamaTokenizer, T5Tokenizer
 
 URL = "https://replicate.delivery/pbxt/qkRFtudUXCoAKlntnVLc3dBhRutRoW02L127bU3Q4778emHJA/engine.tar"
 
+
 class Predictor(BasePredictor):
     def setup(self) -> None:
         # print("Downloading model files...")
@@ -18,21 +19,31 @@ class Predictor(BasePredictor):
         # )
         # launch triton server
         # python3 scripts/launch_triton_server.py --world_size=1 --model_repo=/src/tensorrtllm_backend/triton_model
-        subprocess.run(["python3", "/src/tensorrtllm_backend/scripts/launch_triton_server.py", "--world_size=1", "--model_repo=/src/tensorrtllm_backend/triton_model"])
-        
+        subprocess.run(
+            [
+                "python3",
+                "/src/tensorrtllm_backend/scripts/launch_triton_server.py",
+                "--world_size=1",
+                # "--model_repo=/src/triton_model",
+                "--model_repo=/src/triton_model_repo",
+            ]
+        )
+
         tokenizer = AutoTokenizer.from_pretrained(
-            "/src/tensorrtllm_backend/tensorrt_llm/examples/gpt/gpt2",
-            padding_side='left',
-            trust_remote_code=True
+            "/src/gpt2",
+            padding_side="left",
+            trust_remote_code=True,
         )
 
         self.client = TRTLLMClient(tokenizer=tokenizer)
-        
+
     def predict(
         self,
         prompt: str = Input(description="Enter a prompt", default=""),
     ) -> ConcatenateIterator:
-
+        output = ""
         for decoded_token in self.client.run(text=prompt):
+            output += decoded_token
             yield decoded_token
 
+        print(output)
