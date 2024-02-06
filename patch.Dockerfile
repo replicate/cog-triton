b685f841947d6f34b56acc08fe4b1ada42c0e798
technillogue <technillogue@gmail.com>
2024-02-06 17:29:03 -0500

    use generate_stream endpoint with an sse client

--- patch.Dockerfile
+++ patch.Dockerfile
@@ -1,4 +1,5 @@
 FROM r8.im/replicate-internal/staging-gpt2-triton-trt-llm@sha256:69668ff28d97a066bc36621cc11820d7c8adb5ddc9fe32df963705effddd1414
+RUN pip install httpx
 RUN pip install https://r2.drysys.workers.dev/tmp/cog-0.10.0a4.dev65+g511a4a7.d20240205-py3-none-any.whl 
 COPY ./predict.py /src/predict.py
 RUN ln -sf $(which echo) $(which pip)
--- predict.py
+++ predict.py
@@ -2,31 +2,18 @@
 # https://github.com/replicate/cog/blob/main/docs/python.md
 import asyncio
 import os
-import queue
 import subprocess
-import threading
 
 from cog import BasePredictor, ConcatenateIterator, Input, Path
-from transformers import AutoTokenizer, LlamaTokenizer, T5Tokenizer
-
-from trtllm_client import TRTLLMClient
 from utils import maybe_download_tarball_with_pget
+import httpx
+
 
 URL = "https://replicate.delivery/pbxt/qkRFtudUXCoAKlntnVLc3dBhRutRoW02L127bU3Q4778emHJA/engine.tar"
 
 
-class FakeQueue(asyncio.Queue):
-    put = asyncio.Queue.put_nowait
-
 class Predictor(BasePredictor):
     def setup(self, weights: Path = None) -> None:
-        # print("Downloading model files...")
-
-        # get tokenizer_dir from os.environ
-        tokenizer_dir = os.environ.get(
-            "TOKENIZER_DIR", "/src/triton_model_repo/tensorrt_llm/1/"
-        )
-
         engine_dir = os.environ.get(
             "ENGINE_DIR", "/src/triton_model_repo/tensorrt_llm/1/"
         )
@@ -43,11 +30,9 @@ class Predictor(BasePredictor):
             print("Engine directory is empty. Exiting.")
             self.model_exists = False
             return
-        else:
-            self.model_exists = True
+        self.model_exists = True
         # # launch triton server
         # # python3 scripts/launch_triton_server.py --world_size=1 --model_repo=/src/tensorrtllm_backend/triton_model
-
         subprocess.run(
             [
                 "python3",
@@ -56,84 +41,22 @@ class Predictor(BasePredictor):
                 "--model_repo=/src/triton_model_repo",
             ]
         )
+        self.client = httpx.AsyncClient()
 
-        # Maybe need to special case llama tokenizer loading
-        # should probably pull this out of setup so we can support
-        # variety of contexts.
-
-        self.tokenizer = AutoTokenizer.from_pretrained(
-            tokenizer_dir,
-            padding_side="left",
-            trust_remote_code=True,
-        )
-
-
-    # def token_emitter(self):
-    #     while True:
-    #         # Wait for the callback to put a result in the queue
-    #         try:
-    #             result = self.client.user_data._completed_requests.get(timeout=0.1)
-    #         except queue.Empty:
-    #             continue  # Continue waiting for the callback
-
-    #         # Try to get the decoded tokens
-    #         try:
-    #             decoded_token = self.client.user_data._decoded_tokens.get_nowait()
-    #             print(decoded_token)
-    #             yield decoded_token
-    #             print("wtf now")
-    #         except queue.Empty:
-    #             pass  # No decoded tokens available yet
-
-    async def predict(self, prompt: str) -> ConcatenateIterator:
-        async for token in self._predict(str):
-            yield token
-
-    async def _predict(self, prompt: str):
+    async def predict(
+        self, prompt: str, max_new_tokens: int = 500
+    ) -> ConcatenateIterator:
         if not self.model_exists:
             print(
                 "Your model directory is empty, so there's nothing to do. Remember, you can't run this like a normal model. You need to YOLO!"
             )
             return
-        client = TRTLLMClient(tokenizer=self.tokenizer)
-
-        # Ensure the client is configured with the shared queue
-        client.user_data._completed_requests = (
-            FakeQueue()
-        )  # Reset for each call if necessary
-        client.user_data._decoded_tokens = (
-            FakeQueue()
-        )  # Reset for each call if necessary
-
-        # Start Triton client inference in a separate thread
-        client_thread = threading.Thread(target=lambda: client.run(text=prompt))
-        client_thread.start()
-
-        # Stream tokens as they arrive from Triton
-        while True:
-            try:
-                decoded_token = await client.user_data._decoded_tokens.get(
-                    # timeout=0.1
-                )  # Adjust timeout as needed
-                if decoded_token is None:  # Check for stream termination signal
-                    break
-                yield decoded_token
-            except queue.Empty:
-                break  # Optionally handle timeout or continue waiting
-
-        client_thread.join()  # Ensure client thread has finished
-
-    # def predict(
-    #     self, prompt: str = Input(description="Enter a prompt", default="")
-    # ) -> ConcatenateIterator:
-    #     # Start the client in a separate thread to avoid blocking
-    #     client_thread = threading.Thread(target=lambda: self.client.run(text=prompt))
-    #     client_thread.start()
-
-    #     # Emit tokens as they arrive
-    #     for token in self.token_emitter():
-    #         yield token
-
-    #     # Optionally, signal the token emitter to stop by putting None into the queue
-    #     self.client.user_data.put(None)
-    #     client_thread.join()
+        args = {"text_input": prompt, "max_tokens": max_new_tokens, "stream": True}
+        req = client.stream(
+            "POST",
+            "http://localhost:8000/v2/models/ensemble/generate_stream",
+            json=args,
+        )
+        async with req as resp:
+            async for event in receive_sse(req):
+                yield event.json()["text_output"]
