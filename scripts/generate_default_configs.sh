70316e407ec99acf11df7b4899f82e101c8cca84
joe <joe@replicate.com>
2024-02-14 22:04:37 +0000

    add max batch size and bls config

--- scripts/generate_default_configs.sh
+++ scripts/generate_default_configs.sh
@@ -9,6 +9,7 @@ if [ ! -d "./triton_model_repo" ]; then
     cp -r ./triton_templates/preprocessing triton_model_repo/
     cp -r ./triton_templates//postprocessing triton_model_repo/
     cp -r ./triton_templates/tensorrt_llm triton_model_repo/
+    cp -r ./triton_templates/tensorrt_llm_bls triton_model_repo/
 fi
 
 # Use the triton_fill_template.py script to generate the config.pbtxt files, overwriting the defaults
@@ -16,23 +17,29 @@ fi
 
 # get tokenizer_dir from TOKENIZER_DIR env variable, set default to /src/triton_model_repo/tensorrt_llm/1
 TOKENIZER_DIR=${TOKENIZER_DIR:-/src/triton_model_repo/tensorrt_llm/1/}
+MAX_BATCH_SIZE=${MAX_BATCH_SIZE:-64}
 
 # Generate preprocessing config
 python3 scripts/triton_fill_template.py triton_templates/preprocessing/config.pbtxt \
-     "triton_max_batch_size:4,tokenizer_dir:${TOKENIZER_DIR},tokenizer_type:auto,preprocessing_instance_count:1" \
+     "triton_max_batch_size:${MAX_BATCH_SIZE},tokenizer_dir:${TOKENIZER_DIR},tokenizer_type:auto,preprocessing_instance_count:${MAX_BATCH_SIZE:-64}" \
      > ./triton_model_repo/preprocessing/config.pbtxt
 
 # Generate tensorrt_llm config
 python3 scripts/triton_fill_template.py triton_templates/tensorrt_llm/config.pbtxt \
-    "triton_max_batch_size:4,decoupled_mode:True,engine_dir:/src/triton_model_repo/tensorrt_llm/1,batching_strategy:inflight_fused_batching,batch_scheduler_policy:max_utilization,max_queue_delay_microseconds:100" \
+    "triton_max_batch_size:${MAX_BATCH_SIZE},decoupled_mode:True,engine_dir:/src/triton_model_repo/tensorrt_llm/1,batching_strategy:inflight_fused_batching,batch_scheduler_policy:max_utilization,max_queue_delay_microseconds:100" \
      > ./triton_model_repo/tensorrt_llm/config.pbtxt
 
 # Generate postprocessing config
 python3 scripts/triton_fill_template.py triton_templates/postprocessing/config.pbtxt \
-    "triton_max_batch_size:4,tokenizer_dir:${TOKENIZER_DIR},tokenizer_type:auto,postprocessing_instance_count:1" \
+    "triton_max_batch_size:${MAX_BATCH_SIZE},tokenizer_dir:${TOKENIZER_DIR},tokenizer_type:auto,postprocessing_instance_count:${MAX_BATCH_SIZE:-64}" \
      > ./triton_model_repo/postprocessing/config.pbtxt
 
 # Generate ensemble config
 python3 scripts/triton_fill_template.py triton_templates/ensemble/config.pbtxt \
-    "triton_max_batch_size:4" \
+    "triton_max_batch_size:${MAX_BATCH_SIZE}" \
+     > ./triton_model_repo/ensemble/config.pbtxt
+
+# Generate tensorrt_llm_bls config
+python3 scripts/triton_fill_template.py triton_templates/ensemble/config.pbtxt \
+    "triton_max_batch_size:${MAX_BATCH_SIZE},decoupled_mode:True,bls_instance_count:${MAX_BATCH_SIZE},accumulate_tokens:true" \
      > ./triton_model_repo/ensemble/config.pbtxt
