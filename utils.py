05534626c3bb4ee1ae5d16b126ff39fed05890ac
joe <joe@replicate.com>
2024-02-16 15:04:00 +0000

    if model assets are already present, don't download them again

--- utils.py
+++ utils.py
@@ -36,7 +36,16 @@ def maybe_download_tarball_with_pget(
 
     """
 
-    print("Downloading weights...")
+    # if dest exists and is not empty, return
+    if os.path.exists(dest) and os.listdir(dest):
+        print(f"Files already present in the `{dest}`, nothing will be downloaded.")
+        return dest
+
+    # if dest exists but is empty, remove it so we can pull with pget
+    if os.path.exists(dest):
+        shutil.rmtree(dest)
+
+    print("Downloading model assets...")
     command = ["pget", url, dest, "-x"]
     subprocess.check_call(command, close_fds=True)
 
