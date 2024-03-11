7ef6fb37bed543a237125382cac223a1ef8422a6
joe <joe@replicate.com>
2024-03-11 19:25:49 +0000

    working on tests

--- scripts/triton_config_generator.py
+++ scripts/triton_config_generator.py
@@ -52,82 +52,18 @@ def get_config_paths(model_config, model):
     return template_file, output_file
 
 
-def get_default_args(model):
+def generate_configs(config):
+    models = ['preprocessing', 'tensorrt_llm', 'postprocessing', 'ensemble', 'tensorrt_llm_bls']
 
-    ensemble_args = {}
-
-    preprocessing_args = {
-        'tokenizer_dir': '/src/triton_model_repo/tensorrt_llm/1/',
-        'tokenizer_type': 'auto',
-    }
-    postprocessing_args = {
-        'tokenizer_dir': '/src/triton_model_repo/tensorrt_llm/1/',
-        'tokenizer_type': 'auto',
-    }
-    tensorrt_llm_args = {
-        'engine_dir': '/src/triton_model_repo/tensorrt_llm/1/',
-        'decoupled_mode': 'True',
-        'batching_strategy': 'inflight_fused_batching',
-        'max_queue_delay_microseconds': 100,
-        'batch_scheduler_policy': 'max_utilization'
-    }
-    tensorrt_llm_bls_args = {
-        'decoupled_mode': 'True',
-        'accumulate_tokens': 'true',
-    }
-
-    default_args = {
-        'preprocessing': preprocessing_args,
-        'postprocessing': postprocessing_args,
-        'tensorrt_llm': tensorrt_llm_args,
-        'tensorrt_llm_bls': tensorrt_llm_bls_args,
-        'ensemble': ensemble_args
-    }
-
-
-    if model in default_args:
-        return default_args[model]
-    else:
-        raise ValueError(f"Model {model} not found in default args. Must be one of {list(default_args.keys())}")
-
-def populate_max_batch_size_and_instance_count(config, max_batch_size, default_models):
-    for model in default_models:
+    for model in models:
         if model not in config:
-            config[model] = {'args': {}}
-
-        config[model]['args']['triton_max_batch_size'] = max_batch_size
-        
-        if model in ["ensemble", "tensorrt_llm"]:
             continue
-        elif model == 'tensorrt_llm_bls':
-            instance_count_key = "bls_instance_count"
-        else:
-            instance_count_key = f"{model}_instance_count"
-
-        config[model]['args'][instance_count_key] = max_batch_size
-
-    return config
-
-def main(yaml_file):
-    config = load_yaml_config(yaml_file)
-    max_batch_size = config.get('max_batch_size')
-    default_models = ['preprocessing', 'tensorrt_llm', 'postprocessing', 'ensemble', 'tensorrt_llm_bls']
-
-     
-    if max_batch_size:
-        config = populate_max_batch_size_and_instance_count(config, max_batch_size, default_models)
-
-
-    for model in default_models:
-        if model not in config:
-            config[model] = {'args': {}}
 
         model_config = config[model]
 
         template_file, output_file = get_config_paths(model_config, model)
 
-        default_args = get_default_args(model)
-        substitutions = {**default_args, **model_config.get('args', {})}
+        substitutions = model_config.get('args', {})
 
         filled_config = fill_template(template_file, substitutions)
         write_config(filled_config, output_file)
@@ -135,6 +71,9 @@ def main(yaml_file):
     print("Serving with config:")
     print(config)
 
+def main(yaml_file):
+    config = load_yaml_config(yaml_file)
+    generate_configs(config)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Fill Triton config templates based on YAML configuration.')
---
+++ tests/unit/test_triton_config_generator.py
@@ -0,0 +1,17 @@
+import sys
+sys.path.append(".")
+
+from scripts.triton_config_generator import generate_configs, load_yaml_config
+ 
+def test_load_yaml_config():
+    config = load_yaml_config("./tests/assets/default_triton_config.yaml")
+    assert config == {'preprocessing': {'args': {'tokenizer_dir': '/src/triton_model_repo/tensorrt_llm/1/', 'tokenizer_type': 'auto', 'triton_max_batch_size': 64, 'preprocessing_instance_count': 64}}, 'tensorrt_llm': {'args': {'engine_dir': '/src/triton_model_repo/tensorrt_llm/1/', 'triton_max_batch_size': 64, 'decoupled_mode': True, 'batching_strategy': 'inflight_fused_batching', 'batch_scheduler_policy': 'max_utilization', 'max_queue_delay_microseconds': 100}}, 'postprocessing': {'args': {'tokenizer_dir': '/src/triton_model_repo/tensorrt_llm/1/', 'tokenizer_type': 'llama', 'triton_max_batch_size': 64, 'postprocessing_instance_count': 64}}, 'ensemble': {'args': {'triton_max_batch_size': 64}}, 'tensorrt_llm_bls': {'args': {'triton_max_batch_size': 64, 'decoupled_mode': True, 'bls_instance_count': 64, 'accumulate_tokens': 'true'}}}
+
+def test_generate_configs():
+    config = load_yaml_config("./tests/assets/default_triton_config.yaml")
+    for key in config:
+        config[key]['template'] = f'./tests/assets/triton_templates/{key}/config.pbtxt'
+        config[key]['output'] = f'./tests/assets/triton_model_repo/{key}/config.pbtxt'
+        
+    generate_configs(config)
+    assert True
