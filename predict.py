05de91412ae8f0bae56466b9ac4a886b01b868cf
joe <joe@replicate.com>
2024-03-16 04:13:44 +0000

    gpt2 working, tp broken

--- predict.py
+++ predict.py
@@ -5,7 +5,7 @@ import subprocess
 import time
 import httpx
 from cog import BasePredictor, ConcatenateIterator, Input
-
+import torch
 from sse import receive_sse
 from triton_config_generator import generate_configs, load_yaml_config
 
@@ -16,25 +16,84 @@ from utils import (
     StreamingTokenStopSequenceHandler,
 )
 
+import json
+import numpy as np
+from typing import AsyncGenerator, List, Any
+from pathlib import Path
+from tensorrt_llm.executor import ParallelGenerationExecutor, GenerationExecutor#, GenerationRequest
+from tensorrt_llm.hlapi.tokenizer import TransformersTokenizer
+from transformers import AutoTokenizer
+import tensorrt_llm.bindings as tllm
+
+
+class GenerationRequest:
+
+    def __init__(self,
+                 req_id: int,
+                 ids: torch.Tensor,
+                 end_id: int,
+                 pad_id: int,
+                 streaming: bool = True,
+                 **kwargs):
+        self.prompt = None
+        self.ids = ids
+        self.streaming = streaming
+        self.kwargs = kwargs
+        self.end_id = end_id
+        self.pad_id = pad_id
+        self._id = req_id
+
+    def get_inference_request(self) -> tllm.InferenceRequest:
+        ir = tllm.InferenceRequest(self._id)
+        ir.input_ids = self.ids.to(dtype=torch.int32)
+        ir.is_streaming = self.streaming
+
+        def set_property(name: str,
+                         dtype: torch.dtype = torch.int32,
+                         default: Any = None):
+            if name in self.kwargs or default is not None:
+                value = self.kwargs.get(name, default)
+                setattr(ir, name, torch.tensor([value], dtype=dtype))
+
+        set_property("max_new_tokens", default=[8])
+
+        set_property("end_id", default=self.end_id)
+        set_property("pad_id", default=self.pad_id)
+
+        set_property("min_length")
+        set_property("temperature", torch.float32)
+        set_property("runtime_top_k", torch.int32)
+        set_property("runtime_top_p", torch.float32)
+        set_property("random_seed", torch.int64)
+
+        if 'stop_words_list' in self.kwargs:
+            stop_words_tensor = torch.tensor(self.kwargs['stop_words_list'], dtype=torch.int32)
+            setattr(ir, 'stop_words_list', stop_words_tensor)
+
+        return ir
+
+
 
 class Predictor(BasePredictor):
     async def setup(self, weights: str = "") -> None:
+        engine_dir = os.environ.get(
+            "ENGINE_DIR", "/src/triton_model_repo/tensorrt_llm/1/"
+        )
+
+        if not os.listdir(engine_dir):
+            print("Engine directory is empty. Exiting.")
+            self.model_exists = False
+            return
+        self.model_exists = True
+
         COG_TRITON_CONFIG = os.getenv("COG_TRITON_CONFIG", "config.yaml")
         if not os.path.exists(COG_TRITON_CONFIG):
             print(f"Config file {COG_TRITON_CONFIG} not found. Defaults will be used.")
         else:
             print(f"Loading cog-triton config from {COG_TRITON_CONFIG}")
             config = load_yaml_config(COG_TRITON_CONFIG)
-            print("----------------------")
-            print("cog-triton config:")
-            print(config)
-            print("----------------------")
-            if "server" in config["instantiate"]:
-                generate_configs(config["instantiate"]["server"])
 
-        engine_dir = os.environ.get(
-            "ENGINE_DIR", "/src/triton_model_repo/tensorrt_llm/1/"
-        )
+
 
         self.system_prompt_exists = os.getenv("SYSTEM_PROMPT", None)
         self.end_id = os.getenv("END_ID", 2)
@@ -47,50 +106,35 @@ class Predictor(BasePredictor):
                 dest=engine_dir,
             )
 
-        # if engine_dir is empty, stop here
-        if not os.listdir(engine_dir):
-            print("Engine directory is empty. Exiting.")
-            self.model_exists = False
-            return
-        self.model_exists = True
-        self.client = httpx.AsyncClient(timeout=10)
-        for i in range(3):
-            if start_triton():
-                return
-        raise Exception(f"Couldn't start Triton (exit code {self.proc.poll()})")
+        
+        tokenizer_dir = engine_dir
 
-    async def start_triton(self) -> None:
-        # # launch triton server
-        # # python3 scripts/launch_triton_server.py --world_size=1 --model_repo=/src/tensorrtllm_backend/triton_model
-        world_size = os.getenv("WORLD_SIZE", "1")
-        print("Starting Triton")
-        self.proc = subprocess.Popen(
-            [
-                "python3",
-                "/src/launch_triton_server.py",
-                f"--world_size={world_size}",
-                "--log",
-                "--model_repo=/src/triton_model_repo",
-            ],
-            close_fds=False,
+        tokenizer = TransformersTokenizer.from_pretrained(
+            tokenizer_dir,
+            legacy=False,
+            padding_side='left',
+            truncation_side='left',
+            trust_remote_code=True,
+            use_fast=True
         )
-        # Health check Triton until it is ready or for 3 minutes
-        for i in range(180):
-            try:
-                response = await self.client.get(
-                    "http://localhost:8000/v2/health/ready"
-                )
-                if response.status_code == 200:
-                    print("Triton is ready.")
-                    return True
-            except httpx.RequestError:
-                pass
-            await asyncio.sleep(1)
-        print(f"Triton was not ready within 3 minutes (exit code: {self.proc.poll()})")
-        self.proc.terminate()
-        await asyncio.sleep(0.001)
-        self.proc.kill()
-        return False
+
+        world_size=int(os.getenv("WORLD_SIZE", 1))
+        if world_size > 1:
+            self.executor = ParallelGenerationExecutor(
+                tp_size=world_size,
+                engine_dir=engine_dir, 
+                tokenizer=tokenizer,
+                max_beam_width=1,
+            )
+        else:
+            self.executor = GenerationExecutor(
+                engine_dir=engine_dir, 
+                tokenizer=tokenizer
+            )
+
+        
+
+        print("READY!")
 
     async def predict(
         self,
@@ -171,53 +215,37 @@ class Predictor(BasePredictor):
             stop_words=stop_sequences,
             seed=seed,
         )
+        
+        # Args seem to be specified here: https://github.com/NVIDIA/TensorRT-LLM/blob/4bb65f216f2f1dbddb022f4fdd8925c2856baa58/docs/source/inference_request.md?plain=1#L16
 
-        req = self.client.stream(
-            "POST",
-            "http://localhost:8000/v2/models/tensorrt_llm_bls/generate_stream",
-            json=args,
+        request = GenerationRequest(
+            req_id=self.executor.get_next_request_id(),
+            ids=args["input_ids"],
+            streaming=True,
+            max_new_tokens=args["max_tokens"],
+            temperature=args["temperature"],
+            runtime_top_p=args["top_p"],
+            runtime_top_k=int(args["top_k"]),
+            stop_words_list=args["stop_words"],
+            min_length=args["min_length"],
+            pad_id=args["pad_id"],
+            end_id=args["end_id"],
+            random_seed=args["random_seed"]
         )
+        
+        promise = self.executor.submit(request)
 
-        output = ""
-        generation_length = 0
-        stop_sequence_handler = StreamingTokenStopSequenceHandler(
-            stop_sequences=args["stop_words"]
-        )
+        async for output in promise:
+            yield output.text
 
-        async with req as resp:
-            async for event in receive_sse(resp):
-                # Output is the _entire_ sequence, from the beginning
-                try:
-                    output = event.json()["text_output"]
-                # this check can be removed once we identify the cause of KeyError
-                except Exception as e:
-                    raise Exception(f"error with event {event}") from e
-                # Catches partial emojis, waits for them to finish
-                output = output.replace("\N{Replacement Character}", "")
-                # Remove the tokens that were already yielded
-                current_output = output[generation_length:]
-
-                if current_output:
-                    # Process the output for stop sequences
-                    current_output = stop_sequence_handler(current_output)
-                    # If we have a partial stop sequence match or a full match,
-                    # `current_output` will be `None` and we shouldn't yield
-                    if current_output:
-                        yield current_output
-
-                # Update generation length
-                generation_length = len(output)
-
-            # Handles the case where the generation ends in the middle of a valid stop sequence
-            current_output = stop_sequence_handler.finalize()
-            if current_output:
-                yield current_output
 
         self.log(f"Random seed used: `{args['random_seed']}`\n")
         self.log(
             "Note: Random seed will not impact output if greedy decoding is used.\n"
         )
         self.log(f"Formatted prompt: `{formatted_prompt}`")
+
+    
 
     def _process_args(
         self,
@@ -233,11 +261,10 @@ class Predictor(BasePredictor):
         seed: int = None,
         stream: bool = True,
     ):
-        stop_words_list = stop_words.split(",") if stop_words else []
         min_new_tokens = 0 if min_new_tokens is None else min_new_tokens
 
-        pad_id = self.pad_id
-        end_id = self.end_id
+        pad_id = 2
+        end_id = 2
 
         if top_k < 0:
             top_k = 0
@@ -247,8 +274,16 @@ class Predictor(BasePredictor):
         if not seed:
             seed = int(np.random.randint(0, 100000))
 
+        input_ids = self.executor.tokenizer.encode(prompt, return_tensors="pt")
+        max_new_tokens = [max_new_tokens]
+        
+        stop_words_list = stop_words.split(",") if stop_words else []
+
+        stop_words_list = self._to_word_list_format([stop_words_list])
+
+
         args = {
-            "text_input": prompt,
+            "input_ids": input_ids,
             "max_tokens": max_new_tokens,
             "min_length": min_new_tokens,
             "top_k": top_k,
@@ -278,3 +313,56 @@ class Predictor(BasePredictor):
             return formatted_prompt
         formatted_prompt = prompt_template.format(prompt=prompt)
         return formatted_prompt
+
+    def _to_word_list_format(self, word_lists: List[List[str | bytes]]):
+        """
+        word_lists format:
+            len(word_lists) == batch_size
+            word_lists[i] means the words associated to batch item i. A "word" may actually be any string. Like "lorem" or "lorem ipsum".
+        """
+        # assert self.tokenizer != None, "need to set tokenizer"
+        if word_lists is None:
+            # Return an empty array of shape (1,2,0)
+            return np.empty([1, 2, 0], dtype="int32")
+
+        flat_ids = []
+        offsets = []
+        arbitrary_start_sequence_token = "!"
+        arbitrary_start_sequence_id = self.executor.tokenizer.encode(
+            "!", add_special_tokens=False
+        )[0]
+
+        for word_list in word_lists:
+            item_flat_ids = []
+            item_offsets = []
+
+            for word in word_list:
+                if isinstance(word, bytes):
+                    word = word.decode()
+
+                word = arbitrary_start_sequence_token + word
+                ids = self.executor.tokenizer.encode(word, add_special_tokens=False)
+                if ids[0] != arbitrary_start_sequence_id:
+                    raise ValueError(
+                        f"To standardize tokenizer behavior, we prepend '{arbitrary_start_sequence_token}' to the string representation of each stop sequence."
+                        "We then strip the corresponding first token from the stop sequence IDs."
+                        "However, the first token of the stop sequence IDs was not '{arbitrary_start_sequence_id}', which suggestions there is a problem with the tokenizer that you are using."
+                    )
+                else:
+                    ids = ids[1:]
+                if len(ids) == 0:
+                    continue
+
+                item_flat_ids += ids
+                item_offsets.append(len(ids))
+
+            flat_ids.append(np.array(item_flat_ids))
+            offsets.append(np.cumsum(np.array(item_offsets)))
+
+        pad_to = max(1, max(len(ids) for ids in flat_ids))
+
+        for i, (ids, offs) in enumerate(zip(flat_ids, offsets)):
+            flat_ids[i] = np.pad(ids, (0, pad_to - len(ids)), constant_values=0)
+            offsets[i] = np.pad(offs, (0, pad_to - len(offs)), constant_values=-1)
+
+        return np.array([flat_ids, offsets], dtype="int32").transpose((1, 0, 2))
