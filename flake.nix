f428bd99ac390dac077904e69f965a92805696f9
Yorick van Pelt <yorick@yorickvanpelt.nl>
2024-03-28 21:50:50 +0100

    Add temporary substituter

--- flake.nix
+++ flake.nix
@@ -1,5 +1,9 @@
 # syntax = ghcr.io/reproducible-containers/buildkit-nix:v0.1.1@sha256:7d4c42a5c6baea2b21145589afa85e0862625e6779c89488987266b85e088021
 {
+  nixConfig = {
+    extra-trusted-public-keys = "replicate:+uoDlJCmq7Z6lxxacj4tSgsNa24qHqTZTiyRX2CDhsA=";
+    extra-substituters = "https://cache.yori.cc/replicate";
+  };
   inputs = {
     cognix.url = "github:datakami/cognix";
   };
