b786b5264f91e9ae941236cec96cfcb0edac85e3
joe <joe@replicate.com>
2024-04-16 18:27:33 +0000

    update config generator to not use specify tokenizer_type

--- scripts/generate_default_configs.sh
+++ scripts/generate_default_configs.sh
@@ -25,7 +25,7 @@ MAX_BATCH_SIZE=${MAX_BATCH_SIZE:-64}
 
 # Generate preprocessing config
 python3 scripts/triton_fill_template.py triton_templates/preprocessing/config.pbtxt \
-     "triton_max_batch_size:${MAX_BATCH_SIZE},tokenizer_dir:${TOKENIZER_DIR},tokenizer_type:auto,preprocessing_instance_count:${MAX_BATCH_SIZE:-64}" \
+     "triton_max_batch_size:${MAX_BATCH_SIZE},tokenizer_dir:${TOKENIZER_DIR},preprocessing_instance_count:${MAX_BATCH_SIZE:-64}" \
      > ./triton_model_repo/preprocessing/config.pbtxt
 
 # Generate tensorrt_llm config
@@ -35,7 +35,7 @@ python3 scripts/triton_fill_template.py triton_templates/tensorrt_llm/config.pbt
 
 # Generate postprocessing config
 python3 scripts/triton_fill_template.py triton_templates/postprocessing/config.pbtxt \
-    "triton_max_batch_size:${MAX_BATCH_SIZE},tokenizer_dir:${TOKENIZER_DIR},tokenizer_type:auto,postprocessing_instance_count:${MAX_BATCH_SIZE:-64}" \
+    "triton_max_batch_size:${MAX_BATCH_SIZE},tokenizer_dir:${TOKENIZER_DIR},postprocessing_instance_count:${MAX_BATCH_SIZE:-64}" \
      > ./triton_model_repo/postprocessing/config.pbtxt
 
 # Generate ensemble config
